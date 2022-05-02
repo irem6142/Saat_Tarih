5f0a58cebf571ad2ea3f0dcb106f12bfca3f342e
İrem Uslu <83772404+irem6142@users.noreply.github.com>
2022-05-02 23:43:31 +0300

    js.js

--- js.js
+++ js.js
@@ -14,20 +14,23 @@ setInterval(function() {
     minutes.innerHTML = dakika
     seconds.innerHTML = saniye
     if (saat >= 8 && saat <= 21) {
-        hour.innerHTML = "&#128578;" + saat + "."
+        hour.innerHTML = "&#128578; &nbsp;" + saat + "."
         minutes.innerHTML = dakika + "."
-        seconds.innerHTML = saniye + "&#128522;"
+        seconds.innerHTML = saniye + " &nbsp;&#128522;"
 
     }
+    if (saat == 8 || saat == 9) {
+        hour.innerHTML = "&#128578; &nbsp;" + "0" + saat + "."
+    }
     if (saat >= 0 && saat < 8) {
-        hour.innerHTML = "&#128564;" + "0" + saat + "."
+        hour.innerHTML = "&#128564; &nbsp;" + "0" + saat + "."
         minutes.innerHTML = dakika + "."
-        seconds.innerHTML = saniye + "&#128564;"
+        seconds.innerHTML = saniye + " &nbsp;&#128564;"
     }
     if (saat >= 22) {
-        hour.innerHTML = "&#128564;" + saat + "."
+        hour.innerHTML = "&#128564; &nbsp;" + saat + "."
         minutes.innerHTML = dakika + "."
-        seconds.innerHTML = saniye + "&#128564; "
+        seconds.innerHTML = saniye + " &nbsp;&#128564; "
     }
     if (dakika < 10 && saat >= 0 && saat < 8) {
         minutes.innerHTML = "0" + dakika + "."
@@ -40,13 +43,13 @@ setInterval(function() {
     }
 
     if (saniye < 10 && saat >= 0 && saat < 8) {
-        seconds.innerHTML = "0" + saniye + "&#128564;"
+        seconds.innerHTML = "0" + saniye + " &nbsp;&#128564;"
     }
     if (saniye < 10 && saat >= 22) {
-        seconds.innerHTML = "0" + saniye + "&#128564;"
+        seconds.innerHTML = "0" + saniye + " &nbsp;&#128564;"
     }
     if (saniye < 10 && saat >= 8 && saat <= 21) {
-        seconds.innerHTML = "0" + saniye + "&#128522;"
+        seconds.innerHTML = "0" + saniye + " &nbsp;&#128522;"
     }
 
 
@@ -56,44 +59,70 @@ setInterval(function() {
     var yil = S.getFullYear()
 
     day.innerHTML = gun
-
     month.innerHTML = ay
     year.innerHTML = yil
-    if (gun < 10) {
-        day.innerHTML = "0" + gun
-    }
-    if (ay >= 1 && ay <= 2) {
+
+    if (ay >= 1 && ay <= 2 && gun < 10) {
+
         month.innerHTML = "0" + ay
-        day.innerHTML = "&#9924;" + gun
-        year.innerHTML = yil + "&#9924;"
-    }
-    if (ay == 12) {
-        day.innerHTML = "&#9924;" + gun
-        year.innerHTML = yil + "&#9924;"
-    }
-    if (ay >= 3 && ay <= 5) {
-        day.innerHTML = "&#127800;" + gun
+        day.innerHTML = "&#9924; &nbsp;" + "0" + gun
+        year.innerHTML = yil + " &nbsp;&#9924;"
+    } else if (ay == 12 && gun < 10) {
+
+        day.innerHTML = "&#9924;&nbsp;" + "0" + gun
+        year.innerHTML = yil + " &nbsp;&#9924;"
+    } else if (ay >= 3 && ay <= 5 && gun < 10) {
+        day.innerHTML = "&#127800; &nbsp;" + "0" + gun
         month.innerHTML = "0" + ay
 
-        year.innerHTML = yil + "&#127794;"
-    }
-    if (ay >= 6 && ay <= 8) {
-        day.innerHTML = "&#127774;" + gun
-        month.innerHTML = "0" + ay
-        year.innerHTML = yil + "&#127774;"
+        year.innerHTML = yil + " &nbsp;&#127794;"
+    } else if (ay >= 6 && ay <= 8 && gun < 10) {
 
-    }
-    if (ay == 9) {
-        day.innerHTML = "&#127809;" + gun
+        day.innerHTML = "&#127774; &nbsp;" + "0" + gun
         month.innerHTML = "0" + ay
-        year.innerHTML = yil + "&#127810;"
-    }
-    if (ay >= 10 && ay <= 11) {
-        day.innerHTML = "	&#127809;" + gun
+        year.innerHTML = yil + " &nbsp;&#127774;"
+
+    } else if (ay == 9 && gun < 10) {
+
+        day.innerHTML = "&#127809;&nbsp;" + "0" + gun
+        month.innerHTML = "0" + ay
+        year.innerHTML = yil + "&nbsp;&#127810;"
+    } else if (ay >= 10 && ay <= 11 && gun < 10) {
+
+        day.innerHTML = "&#127809;&nbsp;" + "0" + gun
         month.innerHTML = ay
-        year.innerHTML = yil + "&#127810;"
+        year.innerHTML = yil + "&nbsp;&#127810;"
 
     }
+    //Gün 10 dan büyük olduğunda 
+    else if (ay >= 1 && ay <= 2) {
+        month.innerHTML = "0" + ay
+        day.innerHTML = "&#9924;&nbsp;" + gun
+        year.innerHTML = yil + " &nbsp;&#9924;"
+    } else if (ay == 12) {
+        day.innerHTML = "&#9924; &nbsp;" + gun
+        year.innerHTML = yil + " &nbsp;&#9924;"
+    } else if (ay >= 3 && ay <= 5) {
+        day.innerHTML = "&#127800; &nbsp;" + gun
+        month.innerHTML = "0" + ay
+
+        year.innerHTML = yil + " &nbsp;&#127794;"
+    } else if (ay >= 6 && ay <= 8) {
+        day.innerHTML = "&#127774; &nbsp;" + gun
+        month.innerHTML = "0" + ay
+        year.innerHTML = yil + " &nbsp;&#127774;"
+
+    } else if (ay == 9) {
+        day.innerHTML = "&#127809; &nbsp;" + gun
+        month.innerHTML = "0" + ay
+        year.innerHTML = yil + " &nbsp;&#127810;"
+    } else if (ay >= 10 && ay <= 11) {
+        day.innerHTML = "	&#127809; &nbsp;" + gun
+        month.innerHTML = ay
+        year.innerHTML = yil + " &nbsp;&#127810;"
+
+    }
+
 
 
 
